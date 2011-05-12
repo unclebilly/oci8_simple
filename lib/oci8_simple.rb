2a22d88706e6bd0010bea82b4986a630e9f4c94c
Billy Reisinger <billy.reisinger@gmail.com>
2011-05-12 08:41:22 -0500

    Initial implementation

---
+++ lib/oci8_simple.rb
@@ -0,0 +1,138 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+gem 'ruby-oci8'
+require 'oci8'
+require 'pp'
+require 'bigdecimal'
+require 'yaml'
+
+# Run single statements against an arbitrary Oracle schema. This client is intended to be used by simple
+# command-line scripts to aid automation.  This is *not* meant to replace an ORM such as ActiveRecord + OracleEnhancedAdapter.
+# The only prerequisite to running this code is that you have installed the ruby-oci8 gem on your machine. 
+# == Configuration
+# To configure environments and schema settings, edit the 
+# database.yml file in ~/.oci8_simple/
+#   development:
+#     database: oracle.hostname:1521/sid
+#     username: foo_dev
+#     password: OMG333
+#   test:
+#     database: oracle.hostname:1521/sid
+#     username: foo_test
+#     password: OMG333
+#
+# == Logging
+# All logging is done to ~/.oci8_simple/oci8_simple.log
+# == Examples 
+# * Initialize a client against the development schema
+#     client = Oci8Simple.new
+# * Run a simple select query against development schema
+#     client.run('select id, name from foos') => [[2, "lol"], [3, "hey"], ...]
+# * Run a simple select query against stage schema
+#     Oci8Simple.new("stage").run('select id, name from foos') => [[2, "lol"], [3, "hey"], ...]
+# * Update something
+#     client.run <<-SQL
+#       UPDATE foos SET bar='baz' WHERE id=1233
+#     SQL
+# * Run some DDL
+#     client.run <<-SQL
+#       CREATE TABLE foos (
+#          ID NUMBER(38) NOT NULL
+#       )
+#     SQL
+# * Run some PL/SQL
+#     client.run <<-SQL
+#       DECLARE
+#         a NUMBER;
+#         b NUMBER;
+#       BEGIN
+#         SELECT e,f INTO a,b FROM T1 WHERE e>1;
+#         INSERT INTO T1 VALUES(b,a);
+#       END;
+#     SQL
+class Oci8Simple
+  USER_DIR = File.join(ENV["HOME"], ".oci8_simple")
+  CONFIG_FILE = File.join(USER_DIR, "database.yml")
+  LOG_FILE = File.join(USER_DIR, "oci8_simple.log")
+  
+  attr_accessor :log_file, :puts_mode
+  
+  # * env is the environment heading in your database.yml file
+  # * uname is the username you want to use (defaults to config["username"])
+  # * puts_mode is whether you want to format the result for printing to a terminal (defaults to false)
+  def initialize(env, uname=nil, puts_mode=false)
+    @env = env
+    @uname = uname
+    @puts_mode = puts_mode
+    conn.autocommit = true
+  end
+
+  def log_file
+    @log_file ||= File.open(LOG_FILE, 'a')
+  end
+  
+  def run(sql)
+    log(sql)
+    result = []
+    conn.exec(sql) do |r|
+      row = []
+      r.map do |col|
+        if col.class == BigDecimal
+          row << col.to_i
+        elsif col.class == OCI8::CLOB
+          row << col.read
+        else
+          row << col.to_s
+        end
+      end
+      result << row
+    end
+
+    if @puts_mode
+      result.map{|row| row.join(", ")}.join("\n")
+    else
+      if(result.length == 1 && result[0].length == 1)
+        result[0][0]
+      else
+        result
+      end
+    end
+  end
+  
+  def config
+    @config ||= YAML.load_file(CONFIG_FILE)[@env]
+  end
+  
+  private
+    def conn
+      @o ||= OCI8.new(@uname || config["username"], config["password"], config["database"])
+    end
+  
+    def log(str)
+      log_file.puts "#{Time.now} - #{@env} - #{str}"
+    end
+    
+    def self.help
+      <<-HELP
+  Run arbitrary SQL against an Oracle schema. The default schema is the one defined
+  in the development section of your ~/.oci8_simple/database.yml file.
+
+  You do not need to include a semicolon to end a statement. The statement should be enclosed in single
+  or double quotes. 
+
+  Usage: #{$0} SQL [ENV]
+  Example: #{$0} 'select id from users' stage
+  HELP
+    end
+end 
+
+if __FILE__ == $0
+  if(["-h", "--help"].include?(ARGV[0]) || ARGV.empty?)
+    Oci8Simple.help
+  else
+    sql = ARGV[0] || ""
+    env = ARGV[1] || "development"
+    Oci8Simple.new(env).run(sql)
+  end
+end
--- test/test_oci8_simple.rb
+++ test/test_oci8_simple.rb
@@ -1,7 +1,53 @@
 require 'helper'
 
 class TestOci8Simple < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "Given a table" do
+    setup do
+      @client = Oci8Simple.new("test", nil, false)
+      @client.run "DROP TABLE OCI8_SIMPLE_TEST CASCADE CONSTRAINTS" rescue nil
+      @client.run <<-SQL
+        CREATE TABLE "OCI8_SIMPLE_TEST"
+          (
+            "ID"             NUMBER(38,0) NOT NULL ENABLE,
+            "NAME"           VARCHAR2(400 CHAR) NOT NULL ENABLE,
+            "TEXTS"          CLOB
+          )
+      SQL
+      @client.run <<-SQL
+        INSERT INTO OCI8_SIMPLE_TEST (ID, NAME, TEXTS) VALUES (1, 'Johnny', 'OMG')
+      SQL
+      @client.run <<-SQL
+        INSERT INTO OCI8_SIMPLE_TEST (ID, NAME, TEXTS) VALUES (2, 'Jenny', 'OMG')
+      SQL
+    end
+    context "when puts mode is false the client" do
+      setup do
+      end
+      should "be able to run a simple query with a single result" do
+        assert_equal 2, @client.run("select count(*) from OCI8_SIMPLE_TEST")
+      end
+      should "be able to run a simple query with multiple results and multiple columns" do
+        assert_equal [["1", "Johnny", "OMG"], ["2", "Jenny", "OMG"]], @client.run("select * from OCI8_SIMPLE_TEST")
+      end
+    
+      should "have logged something" do
+        File.unlink(Oci8Simple::LOG_FILE)
+        assert(!File.exists?(Oci8Simple::LOG_FILE))
+        @client = Oci8Simple.new("test", nil, false)
+        @client.run "select NULL from dual"
+        @client.log_file.close
+        assert(File.exists?(Oci8Simple::LOG_FILE))
+        assert(!(0 == File.size(Oci8Simple::LOG_FILE)))
+      end
+    end
+    context "when puts mode is true the client" do
+      setup do
+        @client.puts_mode = true
+      end
+      should "format result" do
+        result = @client.run("select * from OCI8_SIMPLE_TEST")
+        assert_equal "1, Johnny, OMG\n2, Jenny, OMG", result
+      end
+    end
   end
 end
