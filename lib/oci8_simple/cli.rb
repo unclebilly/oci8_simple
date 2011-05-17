abe2ec9c0643206e5ed3f4d74b147e0ab5bcd24d
Billy Reisinger <billy.reisinger@gmail.com>
2011-05-16 21:47:04 -0500

    add bin/oci8_simple and Cli support

--- lib/oci8_simple/cli.rb
+++ lib/oci8_simple/cli.rb
@@ -1,7 +1,8 @@
+require 'optparse'
 module Oci8Simple
   # == Description
-  # A very thin wrapper around Oci8Simple::Client that formats the output
-  # as CSV (suitable for printing on the console)
+  # A very thin wrapper around Oci8Simple::Client that handles ARGV / options and
+  # formats the output in a manner suitable for printing on the console
   # == Usage
   #   cli = Oci8Simple::Cli.new
   #   cli.run "select id, name from foos" # "3, Bacon\n5, Cheese Puffs\n..."
@@ -23,5 +24,26 @@ module Oci8Simple
     def client
       @client ||= Client.new(env)
     end
+    
+    def self.usage
+      "Usage: #{$0} \"SQL\" [ENV]"
+    end
+    
+    def self.run_from_argv
+      o = OptionParser.new do |opt|
+        opt.banner = usage
+        opt.on("-v", "--version", "Show version") do
+          puts "version #{File.read(File.join(File.dirname(__FILE__), '..', '..', 'VERSION'))}"
+          exit
+        end
+      end
+      o.parse!
+      if(ARGV[0].nil?)
+        puts o
+      else
+        puts self.new(ARGV[1]).run(ARGV[0])
+      end
+    end
+    
   end
 end
